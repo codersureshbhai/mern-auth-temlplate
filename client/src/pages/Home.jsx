c6cb6fb8cd66808196a5da433e8acceebbab286f
suresh mahto <apnaladka2023@gmial.com>
2026-09-16 23:37:17 +0530

    Update home page design and styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,17 @@
 function Home() {
-  return <h1>Home Page is here </h1>;
+  return (
+    <div className="home-page">
+      
+      <h2>What We Offer</h2>
+
+      <ul>
+        <li>User Registration</li>
+        <li>User Login</li>
+        <li>Secure Authentication</li>
+        <li>User Dashboard</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
